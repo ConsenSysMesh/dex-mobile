f739e6ec2276867fe54ad12518af58aeea8083ea
Sean Pollock <seanpollock3344@gmail.com>
2017-04-11 12:36:52 -0500

    Main component houses current application view.

---
+++ device/src/components/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import {
+  View,
+  StyleSheet,
+} from 'react-native'
+
+export default class Main extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  rener() {
+    return(
+      <View>
+      </View>
+    )
+  }
+}
