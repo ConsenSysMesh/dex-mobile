cb71a88aa879808cdec0630c24412dc05599d964
Sean Pollock <seanpollock3344@gmail.com>
2017-04-11 13:42:56 -0500

    Handling gesture swipes. Fixing flex layout issue with gesture wrapper.

--- device/src/components/Navigation.js
+++ device/src/components/Navigation.js
@@ -13,43 +13,51 @@ export default class Navigation extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      gestureName: ''
+      gestureName: 'asdf'
     }
+  }
+
+  onSwipe(getsureName, gestureState) {
+    // console.log('gestureName', gestureName, 'gestureState', gestureState)
+    // switch (gestureName) {
+    //   case 'SWIPE_DOWN':
+    //     this.setState(gestureName: gestureName)
+    //     break;
+    //   case 'SWIPE_UP':
+    //     this.setState(gestureName: gestureName)
+    //     break;
+    //   case 'SWIPE_RIGHT':
+    //     this.setState(gestureName: gestureName)
+    //     break;
+    //   case 'SWIPE_LEFT':
+    //     this.setState(gestureName: gestureName)
+    //     break;
+    //   default:
+    //     break;
+    // }
+    console.log('recognized swipe')
   }
 
   render() {
     const config = {
       velocityThreshold: 0.1,
       directionalOffsetThreshold: 80
-    }
-
-    onSwipe(getsureName, gestureState) {
-      switch (gestureName) {
-        case 'SWIPE_DOWN':
-          this.setState(gestureName: gestureName)
-          break;
-        case 'SWIPE_UP':
-          this.setState(gestureName: gestureName)
-          break;
-        case 'SWIPE_RIGHT':
-          this.setState(gestureName: gestureName)
-          break;
-        case 'SWIPE_LEFT':
-          this.setState(gestureName: gestureName)
-          break;
-        default:
-          break;
-      }
-    }
-
+    };
+    console.log('Main(nav)', Main)
     return (
       <GestureRecognizer
-        onSwipe={ (direction, state) => this.onSwipe(direction, state) }
+        onSwipe={ (direction, state) => this.onSwipe(direction, state)}
         config={config}
+        style={{
+          flex: 1
+        }}
       >
-        <Text>{this.state.gestureName}</Text>
-        <Main />
+        <Text>Recieved gesture: {this.state.gestureName}</Text>
       </GestureRecognizer>
+
+
+
+
     )
   }
 }
