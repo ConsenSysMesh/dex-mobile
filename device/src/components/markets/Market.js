2d765288662a9a15bee98d3d386d19b29faeb0d6
Sean Pollock <seanpollock3344@gmail.com>
2017-04-12 15:12:45 -0500

    scaffolding out market view

---
+++ device/src/components/markets/Market.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import {
+  View,
+} from 'react-native'
+
+export default class Market extends Component {
+  constructor() {
+
+  }
+
+  render() {
+
+    return (
+      <View>
+      </View>
+    )
+  }
+}
