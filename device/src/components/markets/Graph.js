e6f89ae5b0c9e57e4b40660b15877d86e546b872
Sean Pollock <seanpollock3344@gmail.com>
2017-04-12 15:17:18 -0500

    adding graph container

---
+++ device/src/components/markets/Graph.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import {
+  View,
+} from 'rect-native'
+
+export default class Graph extends Component {
+  constructor() {
+
+  }
+
+  render() {
+    return (
+      <View />
+    )
+  }
+}
