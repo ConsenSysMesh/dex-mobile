de8c2d3fb813300e4534b9a11bc92737e6354dbb
Sean Pollock <seanpollock3344@gmail.com>
2017-04-13 15:15:20 -0500

    react component for LineGraph

---
+++ device/src/components/graphs/LineGraph.js
@@ -0,0 +1,17 @@
+import { Component } from 'react'
+import {
+  View,
+  StyleSheet,
+}
+
+export default class LineGraph {
+  constructor() {
+    
+  }
+
+  render() {
+    return (
+      <View />
+    )
+  }
+}
