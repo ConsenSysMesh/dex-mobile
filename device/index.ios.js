d65c78b6e56502716c0f8ec0d60026b9e59efc6b
Sean Pollock <seanpollock3344@gmail.com>
2017-04-13 15:35:48 -0500

    trying out react-native lib

--- device/index.ios.js
+++ device/index.ios.js
@@ -3,9 +3,8 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-import { Navigation } from './src/components/index'
+import { Navigation, Market } from './src/components/index'
 import Router from './src/components/Router'
-// console.log('Router', Router)
 import store from './src/store'
 import { Provider, connect } from 'react-redux';
 import React, { Component } from 'react';
@@ -21,7 +20,7 @@ export default class device extends Component {
     return (
       <Provider store={store}>
         <View style={styles.container}>
-          <Router />
+          <Market />
         </View>
       </Provider>
     );
