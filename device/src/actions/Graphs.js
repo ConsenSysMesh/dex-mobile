913e581052527a7b8e0034e7403b2fde911c6400
Sean Pollock <seanpollock3344@gmail.com>
2017-04-13 20:03:09 -0500

    action class for graphs. getPriceData will be a cron to pull in latest from gauss sim.

---
+++ device/src/actions/Graphs.js
@@ -0,0 +1,13 @@
+import Promise from 'bluebird'
+
+export default class Graphs {
+  cosntructor() {
+
+  }
+
+  getPriceData() {
+    return (dispatch) => {
+      console.log('hit get price data method')
+    }
+  }
+}
