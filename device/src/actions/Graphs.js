26ae33bd3479adc8d59978949831212ce3632868
Sean Pollock <seanpollock3344@gmail.com>
2017-04-18 20:34:19 -0500

    fixing websocket error. setting jsonp: false

--- device/src/actions/Graphs.js
+++ device/src/actions/Graphs.js
@@ -3,12 +3,14 @@ import SocketIOClient from 'socket.io-client'
 
 export default class Graphs {
   constructor() {
-    this.socket = SocketIOClient('http://localhost:3000')
+    this.socket = SocketIOClient('http://localhost:6000', {jsonp: false})
+    console.log("this.socket", this.socket)
+    // this.socket.on('price-history', this.dataFeed)
   }
 
   dataFeed() {
-    socket.on('price-history', (message) => {
-      console.log('message', message)
+    socket.on('price-history', (price_obj) => {
+      console.log('price_obj', price_obj)
     })
   }
 
