975001b0fa1dec541d0c3329bcd7a8ae0a5ad950
Sean Pollock <seanpollock3344@gmail.com>
2017-04-18 20:15:50 -0500

    adding websocket channel. data feed for price history

--- device/src/actions/Graphs.js
+++ device/src/actions/Graphs.js
@@ -1,18 +1,27 @@
 import Promise from 'bluebird'
+import SocketIOClient from 'socket.io-client'
 
 export default class Graphs {
   constructor() {
-
+    this.socket = SocketIOClient('http://localhost:3000')
   }
 
-  getPriceData() {
-    return (dispatch) => {
-      fetch('http://localhost:6000/price')
-      .then((respone) => {
-        console.log('response', response)
-      }).catch((error) => {
-        console.log('error')
-      })
-    }
+  dataFeed() {
+    socket.on('price-history', (message) => {
+      console.log('message', message)
+    })
   }
+
+
+
+  // getPriceData() {
+  //   return (dispatch) => {
+  //     fetch('http://localhost:6000/price')
+  //     .then((response) => {
+  //       console.log('response', response)
+  //     }).catch((error) => {
+  //       console.log('error', error)
+  //     })
+  //   }
+  // }
 }
