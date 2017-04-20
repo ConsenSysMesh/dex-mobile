634730014b40d3d57598a6d579a3d7dcc2c210a9
Sean Pollock <seanpollock3344@gmail.com>
2017-04-19 22:01:14 -0500

    switching to es6 syntax

--- server/api/server.js
+++ server/api/server.js
@@ -1,18 +1,22 @@
-var express = require('express');
-var http = require('http')
-var socketio = require('socket.io');
+import express from 'express'
+import http from 'http'
+import socketio from 'socket.io'
 
-var app = express();
-var server = http.Server(app);
-var websocket = socketio(server);
-server.listen(3000, () => console.log('listening on *:3000'));
-
-app.get('/price', (req, res) => {
-  res.end('yo this is the api')
-})
-
+const app = express();
+const server = http.Server(app);
+const websocket = socketio(server);
 
 // The event will be called when a client is connected.
 websocket.on('connection', (socket) => {
   console.log('A client just joined on', socket.id);
 });
+
+// restful endpoints
+app.get('/price', (req, res) => {
+  console.log('req', req)
+  console.log('res', res)
+  // websocket.emit('price', )
+  res.end('yo this is the api')
+})
+
+server.listen(3000, () => console.log('listening on *:3000'))
