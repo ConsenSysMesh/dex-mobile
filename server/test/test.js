848ebfcf4e8b3f729ada82746490b1c2bdf1a4ba
Sean Pollock <seanpollock3344@gmail.com>
2017-04-29 23:10:19 -0500

    simple test to instantiate Market instance

---
+++ server/test/test.js
@@ -0,0 +1,13 @@
+import Market from '../markets/Market'
+import Promise from 'bluebird'
+
+let permuation
+createMarket(tokenA, tokenB) {
+  return new Promise((resolve, reject) => {
+    return Promise.delay(0).then(() => {
+      return permutation = new Market()
+    })
+  })
+}
+
+createMarket()
