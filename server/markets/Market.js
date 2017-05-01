68806a6410b15a7c653ffc24131518d3472eed31
Sean Pollock <seanpollock3344@gmail.com>
2017-04-30 21:10:05 -0500

    succesfully pushed to memory orderbook

--- server/markets/Market.js
+++ server/markets/Market.js
@@ -1,6 +1,42 @@
-export defualt class Market {
+import Promise from 'bluebird'
+
+/**
+ * Market
+ * microservice container for Mob instance
+ */
+export default class Market {
   constructor() {
-    this.sellA = []
-    this.sellB = []
+    // this.tokenA = tokenA
+    // this.tokenB = tokenB
+    this.bookA = []
+    this.bookB = []
+
+    console.log('created Market class, yuhhh')
+  }
+
+  sellA(amount) {
+    console.log('hit sell A')
+    return new Promise((resolve, reject) => {
+      return Promise.delay(0)
+      .then(() => {
+        this.bookA.push(amount)
+        resolve(true)
+      }).catch((err) => {
+        reject(err)
+      })
+    })
+  }
+
+  sellB(amount) {
+    console.log('hit sell B')
+    return new Promise((resolve, reject) => {
+      return Promise.delay(0)
+      .then(() => {
+        this.bookB.push(amount)
+        resolve(true)
+      }).catch((err) => {
+        reject(err)
+      })
+    })
   }
 }
--- server/test/test.js
+++ server/test/test.js
@@ -1,13 +1,47 @@
 import Market from '../markets/Market'
 import Promise from 'bluebird'
 
-let permuation
-createMarket(tokenA, tokenB) {
+let permutation
+
+export function createMarket(tokenA, tokenB) {
   return new Promise((resolve, reject) => {
     return Promise.delay(0).then(() => {
       return permutation = new Market()
+    }).then(() => {
+
+      resolve(true)
+    }).catch((error) => {
+      reject(error)
     })
   })
 }
 
-createMarket()
+export function submitSell() {
+  return new Promise((resolve, reject) => {
+    return permutation.sellA(10)
+    .then(() => {
+      return permutation.sellB(10)
+    }).then(() => {
+      console.log('permutation', permutation)
+      resolve(true)
+    }).catch((err) => {
+      reject(err)
+    })
+  })
+}
+
+export function test() {
+  return new Promise((resolve, reject) => {
+    return Promise.delay(0).then(() => {
+      return createMarket()
+    }).then(() => {
+      return submitSell()
+    }).then(() => {
+      resolve(true)
+    }).catch((error) => {
+      reject(error)
+    })
+  })
+}
+
+test()
