cd07f78d153ec98c7c32686ebecd858b90b5006e
Sean Pollock <seanpollock3344@gmail.com>
2017-05-01 18:06:56 -0500

    adding VolumeLine component

---
+++ device/src/components/markets/VolumeLine.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import {
+  VictoryLine
+} from 'victory-native'
+
+export default class VolumeLine extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <VictoryLine />
+    )
+  }
+}
