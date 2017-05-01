dd24ce581ea292528364d0dcb18735c1d20cd101
Sean Pollock <seanpollock3344@gmail.com>
2017-05-01 18:19:12 -0500

    adding action middleware for volume websocket channel

--- device/src/actions/Graphs.js
+++ device/src/actions/Graphs.js
@@ -3,33 +3,27 @@ import SocketIOClient from 'socket.io-client'
 
 export default class Graphs {
   constructor() {
-
+    this.socket = SocketIOClient('http://localhost:3000', {jsonp: false});
+    this.socket.on('connected', () => {
+      console.log('device connected to server')
+    })
   }
 
   priceDataFeed() {
     return (dispatch) => {
-      this.socket = SocketIOClient('http://localhost:3000', {jsonp: false});
-      this.socket.on('connected', () => {
-        console.log('device connected to server')
-      })
       this.socket.on('price', (price_obj) => {
         console.log('price_obj(device)', price_obj)
         dispatch({ type: 'UPDATE_PRICE_HISTORY', result: price_obj})
       })
-      console.log('socket(device)', this.socket)
     }
   }
 
-
-
-  // getPriceData() {
-  //   return (dispatch) => {
-  //     fetch('http://localhost:6000/price')
-  //     .then((response) => {
-  //       console.log('response', response)
-  //     }).catch((error) => {
-  //       console.log('error', error)
-  //     })
-  //   }
-  // }
+  volumeDataFeed() {
+    return (dispatch) => {
+      this.socket.on('volume', (volume_obj) => {
+        console.log('volume_obj(device)', volume_obj)
+        dispatch({ type: 'UPDATE_VOLUME_HISTORY', result: volume_obj })
+      })
+    }
+  }
 }
