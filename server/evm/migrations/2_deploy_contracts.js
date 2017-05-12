474a0abd3a2e2a1b9429796d819a4d8d7afb46fa
Sean Pollock <seanpollock3344@gmail.com>
2017-05-12 13:04:09 -0500

    migration to link SafeAddSun to Token and deploy Token contract

--- server/evm/migrations/2_deploy_contracts.js
+++ server/evm/migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var SafeAddSub = artifacts.require("./SafeAddSub.sol");
+var Token = artifacts.require("./Token.sol")
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  // deployer.deploy(ConvertLib);
+  // deployer.link(ConvertLib, MetaCoin);
+  // deployer.deploy(MetaCoin);
+  deployer.deploy(SafeAddSub)
+  deployer.link(SafeAddSub, Token)
+  deployer.deploy(Token)
 };
