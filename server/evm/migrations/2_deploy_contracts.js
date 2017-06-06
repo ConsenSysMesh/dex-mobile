a639d0e4a7ef6321881f994acb5e0bbd066162f5
Sean Pollock <seanpollock3344@gmail.com>
2017-06-06 16:17:12 -0400

    Adding migration script to deploy a set of Token contracts.

--- server/evm/migrations/2_deploy_contracts.js
+++ server/evm/migrations/2_deploy_contracts.js
@@ -1,11 +1,41 @@
+var Promise = require('bluebird')
 var SafeAddSub = artifacts.require("./SafeAddSub.sol");
 var Token = artifacts.require("./Token.sol")
 
+const tokens = [
+  {
+    name: 'Gnosis',
+    ticker: 'GNO',
+  },
+  {
+    name: 'Brave',
+    ticker: 'BAT',
+  },
+  {
+    name: 'Golem',
+    ticker: 'GNT',
+  },
+  {
+    name: 'Steemit',
+    ticker: 'STEEM',
+  }
+]
 module.exports = function(deployer) {
   // deployer.deploy(ConvertLib);
   // deployer.link(ConvertLib, MetaCoin);
   // deployer.deploy(MetaCoin);
-  deployer.deploy(SafeAddSub)
-  deployer.link(SafeAddSub, Token)
-  deployer.deploy(Token)
+  return new Promise((resolve, reject) => {
+    return Promise.delay(0)
+    .then(() => {
+      return deployer.deploy(SafeAddSub)
+    }).then(() => {
+      deployer.link(SafeAddSub, Token)
+      return tokens
+    }).map((token) => {
+      console.log('token')
+      // return token.new()
+    })
+  })
+  // deployer.link(SafeAddSub, Token)
+  // deployer.deploy(Token)
 };
